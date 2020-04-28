db89f47910981d62c90b232fab19cfa1aba6f3fb
Albert Anugerah <albertanugerah@hotmail.com>
2020-04-29 03:01:44 +0700

    menambahkan list satu dua

--- index.php
+++ index.php
@@ -8,5 +8,9 @@
 <body>
   <h1>Hellow world</h1>
   <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ex repellendus neque nulla! Corporis sapiente tenetur autem quasi magni nemo amet quibusdam cupiditate saepe sint, fuga eos hic nobis rem illum.</p>
+  <ul>
+    <li>satu</li>
+    <li>dua</li>
+  </ul>
 </body>
 </html>
